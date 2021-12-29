4d84c017294723b9afb92a4ac3b24a700ec27370
PhineasKitunga <81112014+PhineasKitunga@users.noreply.github.com>
2021-12-29 09:54:38 +0200

    Add files via upload

---
+++ lib/constant.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+const colorBackground = Color(0xff2F847C);
+
+const styleHeadings = TextStyle(
+  fontSize: 28,
+  color: Colors.white,
+  fontFamily: 'Montserrat-regular',
+);
+
+const styleText = TextStyle(
+  fontSize: 20,
+  color: Colors.white,
+  fontFamily: 'Montserrat-regular',
+);
