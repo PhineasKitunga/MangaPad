2ca86bbd0a96ed4f8608d30af55836f369e65e40
PhineasKitunga <81112014+PhineasKitunga@users.noreply.github.com>
2021-12-30 18:49:25 +0200

    Add files via upload

---
+++ lib/citations/vengeance.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Vengeance extends StatefulWidget {
+  const Vengeance({Key? key}) : super(key: key);
+
+  @override
+  _VengeanceState createState() => _VengeanceState();
+}
+
+class _VengeanceState extends State<Vengeance> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
