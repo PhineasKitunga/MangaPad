d8ffdfe0cc22016c904e485e2b32a3fa8544679e
PhineasKitunga <81112014+PhineasKitunga@users.noreply.github.com>
2022-01-01 23:43:39 +0200

    Add files via upload

---
+++ lib/citations/amitie.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Amitie extends StatefulWidget {
+  const Amitie({Key? key}) : super(key: key);
+
+  @override
+  _AmitieState createState() => _AmitieState();
+}
+
+class _AmitieState extends State<Amitie> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
