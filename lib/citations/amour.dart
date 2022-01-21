446ab21a9c37ee27b4cdaa6844633815f38900a1
PhineasKitunga <81112014+PhineasKitunga@users.noreply.github.com>
2022-01-21 08:04:41 +0200

    Update amour.dart

--- lib/citations/amour.dart
+++ lib/citations/amour.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:mangapad/constant.dart';
-import 'package:mangapad/app_and_theme.dart';
+import 'list_genre.dart';
 
 class Amour extends StatefulWidget {
   const Amour({Key? key}) : super(key: key);
@@ -15,64 +15,187 @@ class _AmourState extends State<Amour> {
     return MaterialApp(
       theme: ThemeData.dark(),
       home: Scaffold(
-        body: SafeArea(
-          child:
-              //list view
-              SingleChildScrollView(
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              children: <Widget>[
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    IconButton(
-                      onPressed: () {
-                        Navigator.pop(context);
-                      },
-                      icon: Icon(
-                        Icons.arrow_back,
-                        size: 40,
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(
-                  height: 60,
-                  child: ListView.builder(
-                    itemCount: 9,
-                    scrollDirection: Axis.horizontal,
-                    itemBuilder: (context, index) => Container(
-                      height: 30,
-                      margin: EdgeInsets.all(10),
-                      width: 110,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(12),
-                        color: Colors.white,
-                      ),
-                      child: Center(
-                        child: Text(
-                          "Card",
-                          style: textFontBlanc,
-                        ),
-                      ),
-                    ),
-                  ),
-                ),
-                Flexible(
-                  child: ListView.builder(
-                    itemCount: 15,
-                    itemBuilder: (context, index) => ListTile(
-                      title: Text("List $index"),
-                    ),
-                    physics: NeverScrollableScrollPhysics(),
-                    shrinkWrap: true,
-                  ),
-                ),
-              ],
+        appBar: AppBar(
+          leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context);
+            },
+            icon: const Icon(
+              Icons.arrow_back,
+              size: 40,
             ),
+          ),
+        ),
+        body: SafeArea(
+          child: Column(
+            children: <Widget>[
+              Container(
+                height: 65,
+                child: ListView.builder(
+                    scrollDirection: Axis.horizontal,
+                    itemCount: 9,
+                    itemBuilder: (context, index) {
+                      return Container(
+                        padding: const EdgeInsets.all(10),
+                        child: ContainerWidget(
+                            citationText: genreCitation[index][1]),
+                      );
+                    }),
+              ),
+
+              //  seconde list vertically scrollable
+
+              Expanded(
+                  child: ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.all(25),
+                          child: Container(
+                            width: 320,
+                            height: 400,
+                            child: Column(
+                              children: [
+                                Stack(
+                                  children: <Widget>[
+                                    Positioned(
+                                      child: Container(
+                                        width: 380,
+                                        height: 400,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(35.0),
+                                          color:
+                                              Color.fromRGBO(255, 255, 255, 1),
+                                        ),
+                                      ),
+                                    ),
+                                    Positioned(
+                                      top: 0,
+                                      left: 0,
+                                      child: Container(
+                                        width: 400,
+                                        height: 318,
+                                        decoration: BoxDecoration(
+                                          borderRadius:
+                                              BorderRadius.circular(25),
+                                          image: DecorationImage(
+                                              image: AssetImage(
+                                                  'assets/images/shoto.jpeg'),
+                                              fit: BoxFit.fitWidth),
+                                        ),
+                                      ),
+                                    ),
+                                    const Positioned(
+                                      top: 340,
+                                      left: 100,
+                                      child: Text(
+                                        '',
+                                        textAlign: TextAlign.center,
+                                        style: TextStyle(
+                                            color:
+                                                Color.fromRGBO(239, 190, 56, 1),
+                                            fontFamily: 'Montserrat',
+                                            fontSize: 34,
+                                            letterSpacing:
+                                                0 /*percentages not used in flutter. defaulting to zero*/,
+                                            fontWeight: FontWeight.normal,
+                                            height: 1),
+                                      ),
+                                    ),
+                                    Positioned(
+                                      top: 97,
+                                      left: 1,
+                                      child: Container(
+                                        width: 320,
+                                        height: 57,
+                                        child: Stack(
+                                          children: <Widget>[
+                                            Positioned(
+                                              top: 0,
+                                              left: 0,
+                                              child: Container(
+                                                width: 320,
+                                                height: 57,
+                                                decoration: const BoxDecoration(
+                                                  gradient: LinearGradient(
+                                                    begin: Alignment(
+                                                        6.123234262925839e-17,
+                                                        1),
+                                                    end: Alignment(-1,
+                                                        6.123234262925839e-17),
+                                                    colors: [
+                                                      Color.fromRGBO(1, 30, 61,
+                                                          0.6499999761581421),
+                                                      Color.fromRGBO(
+                                                          237,
+                                                          205,
+                                                          35,
+                                                          0.23000000417232513)
+                                                    ],
+                                                  ),
+                                                ),
+                                              ),
+                                            ),
+                                            const Positioned(
+                                              top: 0,
+                                              left: 17,
+                                              width: 400,
+                                              child: Text(
+                                                'Avec de l\’entrainement même un raté peu devenir un geni',
+                                                textAlign: TextAlign.center,
+                                                style: TextStyle(
+                                                    color: Color.fromRGBO(
+                                                        255, 255, 255, 1),
+                                                    fontFamily: 'Montserrat',
+                                                    fontSize: 16,
+                                                    letterSpacing:
+                                                        0 /*percentages not used in flutter. defaulting to zero*/,
+                                                    fontWeight:
+                                                        FontWeight.normal,
+                                                    height: 1),
+                                              ),
+                                            ),
+                                          ],
+                                        ),
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        );
+                      }))
+            ],
           ),
         ),
       ),
     );
   }
 }
+
+class ContainerWidget extends StatelessWidget {
+  final String citationText;
+  const ContainerWidget({required this.citationText});
+  @override
+  Widget build(BuildContext context) {
+    return ButtonTheme(
+      minWidth: 80,
+      height: 50,
+      child: RaisedButton(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(10),
+        ),
+        onPressed: () {},
+        child: Text(
+          citationText,
+          textAlign: TextAlign.center,
+          style: styleText,
+        ),
+        color: const Color(0xff2F847C),
+      ),
+    );
+  }
+}
+
