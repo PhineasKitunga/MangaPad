53f80fef2f790d2a8d8f62a14f5e8895c06a97c1
PhineasKitunga <81112014+PhineasKitunga@users.noreply.github.com>
2022-03-13 17:15:07 +0200

    Add files via upload

---
+++ lib/citations/amour/newAmour1.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+class NewAmour1 extends StatelessWidget {
+  const NewAmour1({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(10.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(15),
+        child: Container(
+          color: Colors.cyan,
+        ),
+      ),
+    );
+  }
+}
