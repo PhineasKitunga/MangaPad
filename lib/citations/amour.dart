34ec4f00380fd508f436b59ae9bbc56e632c01c4
PhineasKitunga <81112014+PhineasKitunga@users.noreply.github.com>
2022-03-16 00:53:27 +0200

    Update amour.dart

--- lib/citations/amour.dart
+++ lib/citations/amour.dart
@@ -1,19 +1,60 @@
 import 'package:flutter/material.dart';
+import 'package:mangapad/citations/amour/newAmour1.dart';
+import 'package:mangapad/citations/amour/newAmour2.dart';
+import 'package:mangapad/citations/amour/newAmour3.dart';
+import 'package:mangapad/citations/autres.dart';
+import 'package:mangapad/citations/force.dart';
+import 'package:mangapad/citations/reve.dart';
+import 'package:mangapad/citations/souffrance.dart';
+import 'package:mangapad/citations/tristesse.dart';
+import 'package:mangapad/citations/vengeance.dart';
+import 'package:mangapad/citations/vie.dart';
 import 'package:mangapad/constant.dart';
 import 'list_genre.dart';
+import 'amitie.dart';
 
 class Amour extends StatefulWidget {
   const Amour({Key? key}) : super(key: key);
-
   @override
   _AmourState createState() => _AmourState();
 }
 
 class _AmourState extends State<Amour> {
+  final List genreCitation = [
+    'Amitie',
+    'Amour',
+    'Autres',
+    'Force',
+    'reve',
+    'souffrance',
+    'tristesse',
+    'vengeance',
+    'vie',
+  ];
+  final List fonctionQuote = [
+    const Amitie(),
+    const Amour(),
+    const Autres(),
+    const Force(),
+    const Reve(),
+    const Souffrance(),
+    const Tristesse(),
+    const Vengeance(),
+    const Vie(),
+  ];
+  final citaion = [
+    {
+      'image': '',
+      'author': '',
+      'anime': '',
+    },
+  ];
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       theme: ThemeData.dark(),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         appBar: AppBar(
           leading: IconButton(
@@ -29,144 +70,44 @@ class _AmourState extends State<Amour> {
         body: SafeArea(
           child: Column(
             children: <Widget>[
-              Container(
-                height: 65,
-                child: ListView.builder(
-                    scrollDirection: Axis.horizontal,
-                    itemCount: 9,
-                    itemBuilder: (context, index) {
-                      return Container(
-                        padding: const EdgeInsets.all(10),
-                        child: ContainerWidget(
-                            citationText: genreCitation[index][1]),
-                      );
-                    }),
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: SizedBox(
+                  height: 40,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: genreCitation.length,
+                      itemBuilder: (context, index) {
+                        return Container(
+                          padding: const EdgeInsets.only(right: 10),
+                          child: ButtonText(
+                            mangaText: genreCitation[index],
+                            quote: fonctionQuote[index],
+                          ),
+                        );
+                      }),
+                ),
               ),
 
               //  seconde list vertically scrollable
 
               Expanded(
-                  child: ListView.builder(
-                      itemCount: 10,
-                      itemBuilder: (context, index) {
-                        return Padding(
-                          padding: const EdgeInsets.all(25),
-                          child: Container(
-                            width: 320,
-                            height: 400,
-                            child: Column(
-                              children: [
-                                Stack(
-                                  children: <Widget>[
-                                    Positioned(
-                                      child: Container(
-                                        width: 380,
-                                        height: 400,
-                                        decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(35.0),
-                                          color:
-                                              Color.fromRGBO(255, 255, 255, 1),
-                                        ),
-                                      ),
-                                    ),
-                                    Positioned(
-                                      top: 0,
-                                      left: 0,
-                                      child: Container(
-                                        width: 400,
-                                        height: 318,
-                                        decoration: BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(25),
-                                          image: DecorationImage(
-                                              image: AssetImage(
-                                                  'assets/images/shoto.jpeg'),
-                                              fit: BoxFit.fitWidth),
-                                        ),
-                                      ),
-                                    ),
-                                    const Positioned(
-                                      top: 340,
-                                      left: 100,
-                                      child: Text(
-                                        '',
-                                        textAlign: TextAlign.center,
-                                        style: TextStyle(
-                                            color:
-                                                Color.fromRGBO(239, 190, 56, 1),
-                                            fontFamily: 'Montserrat',
-                                            fontSize: 34,
-                                            letterSpacing:
-                                                0 /*percentages not used in flutter. defaulting to zero*/,
-                                            fontWeight: FontWeight.normal,
-                                            height: 1),
-                                      ),
-                                    ),
-                                    Positioned(
-                                      top: 97,
-                                      left: 1,
-                                      child: Container(
-                                        width: 320,
-                                        height: 57,
-                                        child: Stack(
-                                          children: <Widget>[
-                                            Positioned(
-                                              top: 0,
-                                              left: 0,
-                                              child: Container(
-                                                width: 320,
-                                                height: 57,
-                                                decoration: const BoxDecoration(
-                                                  gradient: LinearGradient(
-                                                    begin: Alignment(
-                                                        6.123234262925839e-17,
-                                                        1),
-                                                    end: Alignment(-1,
-                                                        6.123234262925839e-17),
-                                                    colors: [
-                                                      Color.fromRGBO(1, 30, 61,
-                                                          0.6499999761581421),
-                                                      Color.fromRGBO(
-                                                          237,
-                                                          205,
-                                                          35,
-                                                          0.23000000417232513)
-                                                    ],
-                                                  ),
-                                                ),
-                                              ),
-                                            ),
-                                            const Positioned(
-                                              top: 0,
-                                              left: 17,
-                                              width: 400,
-                                              child: Text(
-                                                'Avec de l\’entrainement même un raté peu devenir un geni',
-                                                textAlign: TextAlign.center,
-                                                style: TextStyle(
-                                                    color: Color.fromRGBO(
-                                                        255, 255, 255, 1),
-                                                    fontFamily: 'Montserrat',
-                                                    fontSize: 16,
-                                                    letterSpacing:
-                                                        0 /*percentages not used in flutter. defaulting to zero*/,
-                                                    fontWeight:
-                                                        FontWeight.normal,
-                                                    height: 1),
-                                              ),
-                                            ),
-                                          ],
-                                        ),
-                                      ),
-                                    ),
-                                  ],
-                                ),
-                              ],
-                            ),
-                          ),
-                        );
-                      }))
+                child: PageView(
+                  scrollDirection: Axis.vertical,
+                  children: const [
+                    NewAmour1(),
+                    NewAmour2(),
+                    NewAmour3(),
+                  ],
+                ),
+              ),
+
+              //ad banner vertically
+
+              // Container(
+              //   height: 80,
+              //   color: Colors.green,
+              // )
             ],
           ),
         ),
@@ -175,27 +116,57 @@ class _AmourState extends State<Amour> {
   }
 }
 
-class ContainerWidget extends StatelessWidget {
-  final String citationText;
-  const ContainerWidget({required this.citationText});
+// class ContainerWidget extends StatelessWidget {
+//   final List citationText;
+//   const ContainerWidget({required this.citationText});
+//   @override
+//   Widget build(BuildContext context) {
+//     return ButtonTheme(
+//       minWidth: 80,
+//       height: 50,
+//       child: RaisedButton(
+//         shape: RoundedRectangleBorder(
+//           borderRadius: BorderRadius.circular(10),
+//         ),
+//         onPressed: () {},
+//         child: Text(
+//           citationText.toString(),
+//           textAlign: TextAlign.center,
+//           style: styleText,
+//         ),
+//         color: const Color(0xff2F847C),
+//       ),
+//     );
+//   }
+// }
+
+class ButtonText extends StatelessWidget {
+  final String mangaText;
+  final Widget quote;
+  // ignore: use_key_in_widget_constructors
+  const ButtonText({required this.mangaText, required this.quote});
   @override
   Widget build(BuildContext context) {
     return ButtonTheme(
-      minWidth: 80,
-      height: 50,
+      minWidth: 150,
       child: RaisedButton(
         shape: RoundedRectangleBorder(
           borderRadius: BorderRadius.circular(10),
         ),
-        onPressed: () {},
+        onPressed: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(builder: (context) {
+              return quote;
+            }),
+          );
+        },
+        color: const Color(0xff2F847C),
         child: Text(
-          citationText,
-          textAlign: TextAlign.center,
+          mangaText,
           style: styleText,
         ),
-        color: const Color(0xff2F847C),
       ),
     );
   }
 }
-
